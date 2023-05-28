84e7e161d17ea2baf32a88fe813863bdf892fcf6
wildone <max.barrass@gmail.com>
2023-05-28 20:33:04 +1000

    udpate helpers.

---
+++ build.renew.ps1
@@ -0,0 +1,1 @@
+.\bin\localcert.exe -dataDir ${PWD}\data -acceptTerms -localCert ${PWD}\\data\\cert.pem -localKey ${PWD}\\data\\key.pem -forceRenew
--- build.windows.ps1
+++ build.windows.ps1
@@ -1,5 +1,9 @@
 # env GOOS=windows GOARCH=amd64 
 go build -o ${PWD}\bin\localcert.exe .\cmd\localcert\main.go
+if(!$?) {
+    Write-Host "Build failed."
+    exit 1
+}
 dir ${PWD}\bin\
 mkdir data -ErrorAction SilentlyContinue
-.\bin\localcert.exe -dataDir ${PWD}\data -acceptTerms
+.\bin\localcert.exe -dataDir ${PWD}\data -acceptTerms -localCert ${PWD}\\data\\cert.pem -localKey ${PWD}\\data\\key.pem -forceRenew -debug
